f48a5c85b9e6eddbcce4b9833f7e1b242d570917
strlock <strlock@gmail.com>
2022-06-06 10:23:17 +0300

    Frontend vendor

--- app/code/ELogic/Vendors/Model/Attribute/Frontend/Vendor.php
+++ app/code/ELogic/Vendors/Model/Attribute/Frontend/Vendor.php
@@ -6,15 +6,19 @@ use Magento\Eav\Model\Entity\Attribute\Frontend\AbstractFrontend;
 use Magento\Framework\App\ObjectManager;
 use Magento\Framework\DataObject;
 
-
 class Vendor extends AbstractFrontend
 {
-    public function getValue(DataObject $object)
+    /**
+     * @param DataObject $object
+     * @return string
+     */
+    public function getValue(DataObject $object): string
     {
         $value = $object->getData($this->getAttribute()->getAttributeCode());
         $vendorIds = explode(',', $value);
         $vendorsCollection = ObjectManager::getInstance()->create(\ELogic\Vendors\Model\Vendor::class)->getCollection();
         $vendorsCollection->addFieldToFilter('entity_id', ['in' => $vendorIds]);
+        $vendorNames = [];
         foreach ($vendorsCollection as $vendor) {
             $vendorNames[] = $vendor->getName();
         }
